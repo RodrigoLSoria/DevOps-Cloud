575ce8b5a416fe2883261a348396307ef2e55e24
RodrigoLSoria <138790527+RodrigoLSoria@users.noreply.github.com>
2023-12-01 11:39:47 +0100

    Delete lab1 directory

--- lab1/nsg_association.tf
+++
@@ -1,8 +0,0 @@
-
-resource "azurerm_subnet_network_security_group_association" "nsg-association" {
-  for_each = var.nsgs
-
-  subnet_id                 = each.value.subnet_id
-  network_security_group_id = azurerm_network_security_group.nsg[each.key].id
-
-}
